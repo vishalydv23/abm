35824fcb542a3ac87d792c0521bdbfb5c542cac5
Bertie Brown <bertietbrown@gmail.com>
2023-01-30 11:31:08 +0000

    Minor editing changes for readability

--- EVs/streamlit_app/streamlit_tabs.py
+++ EVs/streamlit_app/streamlit_tabs.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import agent_app
 import model_res_dash
-import new_run  
-import new_run_live  
+import new_run
+import new_run_live
 import altair as alt
 from streamlit_option_menu import option_menu
-from  PIL import Image
+from PIL import Image
 
 # def my_theme():
 #     return {
@@ -42,27 +42,31 @@ from  PIL import Image
 
 
 st.set_page_config(layout="wide")
-logo = Image.open(r'branding//cap_logo.png')
-col1, col2 = st.columns( [0.8, 0.2])
+logo = Image.open(r"branding//cap_logo.png")
+col1, col2 = st.columns([0.8, 0.2])
 with col1:
-   choose = option_menu("Team Evlution - EV Simulation", ['Live Run', "Model Results", "Agent Movement",'New Model Run'], 
-                  icons=['shuffle', 'bar-chart', 'pin-map', 'node-plus'],
-                  menu_icon="app-indicator", default_index=0,orientation="horizontal",
-                  styles={
-                        #"container": {"padding": "5!important", "background-color": "#fafafa"},
-                        "icon": {"color": "orange", "font-size": "25px"}, 
-                        #"nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                        #"nav-link-selected": {"background-color": "#02ab21"},
-                        }
-   )
+    choose = option_menu(
+        "Team Evlution - EV Simulation",
+        ["Live Run", "Model Results", "Agent Movement", "New Model Run"],
+        icons=["shuffle", "bar-chart", "pin-map", "node-plus"],
+        menu_icon="app-indicator",
+        default_index=0,
+        orientation="horizontal",
+        styles={
+            # "container": {"padding": "5!important", "background-color": "#fafafa"},
+            "icon": {"color": "orange", "font-size": "25px"},
+            # "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            # "nav-link-selected": {"background-color": "#02ab21"},
+        },
+    )
 with col2:
-   st.image(logo, width=250 )
+    st.image(logo, width=250)
 
-if choose == 'Live Run':
-   new_run_live.gen_app()
-elif choose =="Model Results":
-   model_res_dash.gen_app()
+if choose == "Live Run":
+    new_run_live.gen_app()
+elif choose == "Model Results":
+    model_res_dash.gen_app()
 elif choose == "Agent Movement":
-   agent_app.gen_app_3()
+    agent_app.gen_app_3()
 else:
-   new_run.gen_app()
+    new_run.gen_app()
